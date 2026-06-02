ce3bcec9e15a2b9bac7254462fb3faffbce4d970
Strahinja Kostic <kostic.v.strahinja@gmail.com>
2026-06-02 22:55:20 +0200

    Promena navigacionog menija

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -6,11 +6,11 @@ import whiskeyImage from '../images/whiskey.png';
 
 const NavigationMenu: React.FC = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center gap-2">
-          <Image src={whiskeyImage} alt="Whiskey glass" width={32} height={32} rounded />
+    <Navbar className="app-navbar mx-3 my-2" bg="dark" variant="dark" expand="lg">
+      <Container fluid className="px-0">
+        <Navbar.Brand as={Link} to="/" className="d-flex align-items-center gap-2 ms-0 navbar-brand-custom">
           <span className="nav-brand-oldlatin">ČISTA KAP</span>
+          <Image src={whiskeyImage} alt="Whiskey glass" width={32} height={32} rounded />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
